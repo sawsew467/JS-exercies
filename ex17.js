ff5d7532496851b239e394994b11972460aa92db
sawsew467 <bao.thang.1912@gmail.com>
2022-11-01 14:49:05 +0700

    validate

--- ex17.js
+++ ex17.js
@@ -1,22 +1,74 @@
 let students = [];
 //
+const validate = (obj) => {
+  if (students.some((student) => student.id === obj.id)) {
+    console.log("ID must be unique");
+    return false;
+  }
+  if (obj.name.length < 2) {
+    console.log("Name must be at least 2 characters");
+    return false;
+  }
+  if (+obj.age <= 0 || isNaN(+obj.balance)) {
+    console.log("Age must be a positive number and greater than zero");
+    return false;
+  }
+  if (+obj.balance <= 0 || isNaN(+obj.balance)) {
+    console.log("Balance must be a positive number and greater than zero");
+    return false;
+  }
+  if (["GOOD", "AVERAGE", "WEAK"].includes(obj.type) === false) {
+    console.log("Type must be 'GOOD' or 'AVERAGE' or 'WEAK'");
+    return false;
+  }
+  if (obj.subjects.length <= 0) {
+    console.log("subjects must be at least 1 subject");
+    return false;
+  }
+  return true;
+};
+//
 const create = (obj) => {
-  students.push(obj);
+  try {
+    if (validate(obj) === false) return;
+    students.push(obj);
+  } catch (error) {
+    console.log("Something went wrong");
+  }
 };
 
 const update = (id, obj) => {
-  students = students.map((student) => (student.id === id ? obj : student));
+  try {
+    if (validate(obj) === false) return;
+    students = students.map((student) => (student.id === id ? obj : student));
+  } catch (error) {
+    console.log("Something went wrong", error);
+  }
 };
 
 const deleteOne = (id) => {
+  if (students.some((student) => student.id === id) === false) {
+    console.log("Your ID is not in the students");
+    return;
+  }
   students = students.filter((student) => student.id !== id);
 };
 
 const findById = (id) => {
+  if (students.some((student) => student.id === id) === false) {
+    console.log("Your ID is not in the students");
+    return;
+  }
   return students.filter((student) => student.id === id)[0];
 };
 
 const findAll = (type, page, size) => {
+  if (+page <= 0 || +size <= 0 || isNaN(page) || isNaN(size)) {
+    console.log(
+      "Page and Size must be 2 positive numbers and greater than zero"
+    );
+    return;
+  }
   const subPage = students.filter(
     (student, index) => index >= (page - 1) * size && index < page * size
   );
@@ -30,6 +82,7 @@ const findAll = (type, page, size) => {
     case "WEAK":
       return subPage.filter((student) => student.type === "WEAK");
     default:
+      console.log("Type must be 'GOOD' or 'AVERAGE' or 'WEAK'");
       break;
   }
 };
@@ -71,7 +124,7 @@ create({
   name: "Thao",
   age: "30",
   type: "AVERAGE",
-  balance: 7945,
+  balance: "7945",
   subjects: ["math", "English", "chemistry"],
 });
 
